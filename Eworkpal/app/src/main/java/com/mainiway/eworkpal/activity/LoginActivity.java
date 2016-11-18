d148eda9cadffabbdb0b75a3ea8cb91e61fa4569
zhangshuang <zhangshuang@mainiway.com>
2016-11-18 18:03:27 +0800

    注册模块，注册、创建企业

--- Eworkpal/app/src/main/java/com/mainiway/eworkpal/activity/LoginActivity.java
+++ Eworkpal/app/src/main/java/com/mainiway/eworkpal/activity/LoginActivity.java
@@ -1,16 +1,22 @@
 package com.mainiway.eworkpal.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.mainiway.eworkpal.R;
 import com.mainiway.eworkpal.base.BaseActivity;
+import com.mainiway.eworkpal.listener.OnClickFastListener;
+import com.mainiway.eworkpal.utils.ToastUtils;
 
 /**
  * ===========================================
  * 作    者：zhsh
  * 版    本：1.0
  * 创建日期：2016/11/18.
- * 描    述：
+ * 描    述：登录界面
  * ===========================================
  */
 
@@ -20,6 +26,26 @@ public class LoginActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+        initView();
     }
 
+    private void initView() {
+        findView(R.id.tv_register_enterprises).setOnClickListener(new FastClickListener());
+    }
+
+    private class FastClickListener extends OnClickFastListener {
+
+        @Override
+        public void onFastClick(View v) {
+
+            switch (v.getId()) {
+
+                case R.id.tv_register_enterprises://注册
+                     startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+                    break;
+
+
+            }
+        }
+    }
 }
