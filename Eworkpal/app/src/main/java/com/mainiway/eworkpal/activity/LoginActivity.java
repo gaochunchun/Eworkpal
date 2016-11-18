82900ab89fb6d28bce2b1d9ab0bc283970217293
zhangshuang <zhangshuang@mainiway.com>
2016-11-18 10:59:22 +0800

    登录界面

---
+++ Eworkpal/app/src/main/java/com/mainiway/eworkpal/activity/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.mainiway.eworkpal.activity;
+
+import android.os.Bundle;
+
+import com.mainiway.eworkpal.R;
+import com.mainiway.eworkpal.base.BaseTitleActivity;
+
+/**
+ * ===========================================
+ * 作    者：zhsh
+ * 版    本：1.0
+ * 创建日期：2016/11/18.
+ * 描    述：
+ * ===========================================
+ */
+
+public class LoginActivity extends BaseTitleActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_login);
+    }
+}
