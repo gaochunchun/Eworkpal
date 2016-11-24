9da3141c61b90509b702eb497655e0e2028b78e8
zhangshuang <zhangshuang@mainiway.com>
2016-11-24 17:58:57 +0800

    注册界面，获取验证码和图片验证码逻辑的处理

--- Eworkpal/app/src/main/java/com/mainiway/eworkpal/activity/user/CommonPhoneCodeActivity.java
+++ Eworkpal/app/src/main/java/com/mainiway/eworkpal/activity/user/CommonPhoneCodeActivity.java
@@ -2,7 +2,13 @@ package com.mainiway.eworkpal.activity.user;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.mainiway.eworkpal.R;
@@ -10,8 +16,11 @@ import com.mainiway.eworkpal.base.BaseTitleActivity;
 import com.mainiway.eworkpal.constant.Constants;
 import com.mainiway.eworkpal.listener.OnClickFastListener;
 import com.mainiway.eworkpal.utils.TimeCount;
+import com.mainiway.eworkpal.utils.ToastUtils;
+import com.mainiway.eworkpal.widgets.ImageCodeView;
 
 import static com.mainiway.eworkpal.R.id.tv_register_get_code;
+
 /**
  * ===========================================
  * 作    者：gao_chun
@@ -20,19 +29,23 @@ import static com.mainiway.eworkpal.R.id.tv_register_get_code;
  * 描    述：公用的获取手机验证码（需根据传递的Intent信息设置Title）
  * ===========================================
  */
-public class CommonPhoneCodeActivity extends BaseTitleActivity{
+public class CommonPhoneCodeActivity extends BaseTitleActivity {
 
     private String label;
     private TextView tv_get_code;
+    private RelativeLayout rl_picture_code_layout;
+    private ImageView iv_picture_code;
+    private EditText et_picture_code;
+    private int count;//测试用的点击验证码次数的变量
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_common_phonecode);
         handlerIntent();
-        if (label.equals(Constants.PHONE_CODE_ENTERPRISE)){
+        if (label.equals(Constants.PHONE_CODE_ENTERPRISE)) {
             setTitle("注册");
-        }else if (label.equals(Constants.PHONE_CODE_FORGET_PWD)){
+        } else if (label.equals(Constants.PHONE_CODE_FORGET_PWD)) {
             setTitle("找回密码");
         }
         showBackwardView(true);
@@ -41,9 +54,9 @@ public class CommonPhoneCodeActivity extends BaseTitleActivity{
     }
 
     //获取Intent传递的数据
-    private void handlerIntent(){
+    private void handlerIntent() {
 
-        if(getIntent() != null){
+        if (getIntent() != null) {
             label = getIntent().getStringExtra(Constants.LABEL_PHONE_CODE);
         }
     }
@@ -52,6 +65,14 @@ public class CommonPhoneCodeActivity extends BaseTitleActivity{
         findView(R.id.tv_getcode_next).setOnClickListener(new FastClickListener());
         tv_get_code = findView(R.id.tv_get_code);
         tv_get_code.setOnClickListener(new FastClickListener());
+        rl_picture_code_layout = findView(R.id.rl_picture_code_layout);
+
+        et_picture_code=findView(R.id.et_picture_code);
+        et_picture_code.addTextChangedListener(textWatcher);
+
+        iv_picture_code = findView(R.id.iv_picture_code);
+        iv_picture_code.setImageBitmap(ImageCodeView.getInstance().createBitmap());
+        iv_picture_code.setOnClickListener(new CommonPhoneCodeActivity.FastClickListener());
 
     }
 
@@ -65,27 +86,76 @@ public class CommonPhoneCodeActivity extends BaseTitleActivity{
 
                 case R.id.tv_getcode_next:  //下一步
 
-                    if (label.equals(Constants.PHONE_CODE_ENTERPRISE)){
+                    if (label.equals(Constants.PHONE_CODE_ENTERPRISE)) {
                         //此处跳转到企业创建界面，可能需要携带参数
-                        startActivity(new Intent(CommonPhoneCodeActivity.this,CreateEnterpriseActivity.class));
-                        overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+                        startActivity(new Intent(CommonPhoneCodeActivity.this, CreateEnterpriseActivity.class));
+                        overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
 
-                    }else if (label.equals(Constants.PHONE_CODE_FORGET_PWD)){
+                    } else if (label.equals(Constants.PHONE_CODE_FORGET_PWD)) {
 
                         //此处跳转到找回密码界面，可能需要携带参数
-                        startActivity(new Intent(CommonPhoneCodeActivity.this,ForgetPwdActivity.class));
+                        startActivity(new Intent(CommonPhoneCodeActivity.this, ForgetPwdActivity.class));
 
                     }
 
                     break;
 
                 case R.id.tv_get_code://获取验证码
-                    TimeCount timeCount = new TimeCount(60000, 1000);//60000, 1000
-                    timeCount.setBtn(tv_get_code, "重新获取");
-                    timeCount.start();
+
+                    count++;
+                    if (count > 3) {
+                        rl_picture_code_layout.setVisibility(View.VISIBLE);
+                        tv_get_code.setBackgroundResource(R.drawable.rectangle_27dp_blue);
+                        tv_get_code.setClickable(false);
+                    } else {
+
+                        TimeCount timeCount = new TimeCount(3000, 1000);//60000, 1000
+                        timeCount.setBtn(tv_get_code, "重新获取");
+                        timeCount.start();
+                    }
+                    break;
+                case R.id.iv_picture_code://动态验证码图片
+                    iv_picture_code.setImageBitmap(ImageCodeView.getInstance().createBitmap());
                     break;
 
             }
         }
     }
+
+    /**
+     * Edittext监听事件
+     */
+    private TextWatcher textWatcher=new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+            int visibility = rl_picture_code_layout.getVisibility();
+            if(visibility==0){//返回值为0，visible,弹出图片验证码布局
+                //如果弹出图片验证码，则判断验图片证码正确，获取验证码按钮可点击
+                if(et_picture_code.getText().length()==4){
+                    if(et_picture_code.getText().toString().equalsIgnoreCase(ImageCodeView.getInstance().getCode())){
+                        rl_picture_code_layout.setVisibility(View.GONE);
+                        tv_get_code.setBackgroundResource(R.drawable.rectangle_27dp_blue_selected);
+                        tv_get_code.setClickable(true);
+                        count=0;
+                    }else{
+                        ToastUtils.showToastShort("图片验证码错误");
+                    }
+                }
+
+            }
+
+        }
+    };
+
 }
