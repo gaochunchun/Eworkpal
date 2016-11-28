ad12b40c027ceb6e75ada7e039a8c82f19c5e809
gao_chun <jz_gaochun@163.com>
2016-11-28 16:56:49 +0800

    移除无用styles属性 调整标题栏右侧图标并支持两个图标 编写使用示例

--- Eworkpal/app/src/main/java/com/mainiway/eworkpal/activity/test/MainActivity.java
+++ Eworkpal/app/src/main/java/com/mainiway/eworkpal/activity/test/MainActivity.java
@@ -7,23 +7,27 @@ import android.view.View;
 import com.mainiway.eworkpal.R;
 import com.mainiway.eworkpal.activity.attendance.RightPositionActivity;
 import com.mainiway.eworkpal.base.BaseActivity;
+import com.mainiway.eworkpal.base.BaseTitleActivity;
+import com.mainiway.eworkpal.utils.ToastUtils;
 
 /**
  * 测试主页
  */
 
-public class MainActivity extends BaseActivity implements View.OnClickListener{
+public class MainActivity extends BaseTitleActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_activity_main);
-
+        setTitle("功能测试界面");
+        showBackwardView(true);
+        showForwardView(R.mipmap.ic_back,R.mipmap.ic_back);
     }
 
     @Override
     public void onClick(View v) {
-
+        super.onClick(v);
         switch (v.getId()){
             case R.id.btn_network:
                 startActivity(new Intent(this,TestActivity.class));
@@ -41,5 +45,17 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
                 startActivity(new Intent(this,PullToRefreshUseActivity.class));
                 break;
         }
+    }
+
+    @Override
+    protected void onForward(View forwardView) {
+        super.onForward(forwardView);
+        ToastUtils.showToastCenter("第一个图标");
+    }
+
+    @Override
+    protected void onForward2(View forwardView2) {
+        super.onForward2(forwardView2);
+        ToastUtils.showToastCenter("第二个图标");
     }
 }
