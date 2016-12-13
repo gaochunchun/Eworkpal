9ca772ca93282c9f961161eabd5ad3deda8e7e33
gao_chun <jz_gaochun@163.com>
2016-12-13 15:40:59 +0800

    注释添加

--- Eworkpal/app/src/main/java/com/mainiway/eworkpal/adapter/EnterpriseListAdapter.java
+++ Eworkpal/app/src/main/java/com/mainiway/eworkpal/adapter/EnterpriseListAdapter.java
@@ -6,6 +6,14 @@ import com.mainiway.eworkpal.R;
 import com.mainiway.eworkpal.activity.test.DataServer;
 import com.mainiway.eworkpal.model.Status;
 
+/**
+ * ===========================================
+ * 作    者：gao_chun
+ * 版    本：1.0
+ * 创建日期：2016-11-29.
+ * 描    述：选择企业列表Adapter
+ * ===========================================
+ */
 public class EnterpriseListAdapter extends BaseQuickAdapter<Status, BaseViewHolder> {
 
     public EnterpriseListAdapter() {
