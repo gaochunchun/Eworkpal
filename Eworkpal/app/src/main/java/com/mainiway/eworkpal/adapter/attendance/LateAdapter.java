6dfd216dc4308e42431380aab3f22dc30f73276a
zhangbiaoing <573544005@qq.com>
2016-12-13 09:42:39 +0800

    添加包

---
+++ Eworkpal/app/src/main/java/com/mainiway/eworkpal/adapter/attendance/LateAdapter.java
@@ -0,0 +1,26 @@
+package com.mainiway.eworkpal.adapter.attendance;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by zhangbiao on 2016/12/13 00013.
+ * 迟到适配器
+ */
+
+public class LateAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
