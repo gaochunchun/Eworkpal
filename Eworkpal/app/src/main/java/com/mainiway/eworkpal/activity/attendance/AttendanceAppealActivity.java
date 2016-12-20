c4dbcb499be6c202976a8443de1f2ca9d0c65f1d
zhangshuang <zhangshuang@mainiway.com>
2016-12-20 10:48:59 +0800

    申诉界面添加时间选择器

--- Eworkpal/app/src/main/java/com/mainiway/eworkpal/activity/attendance/AttendanceAppealActivity.java
+++ Eworkpal/app/src/main/java/com/mainiway/eworkpal/activity/attendance/AttendanceAppealActivity.java
@@ -1,9 +1,12 @@
 package com.mainiway.eworkpal.activity.attendance;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.mainiway.eworkpal.R;
 import com.mainiway.eworkpal.base.BaseTitleActivity;
+import com.mainiway.eworkpal.widgets.TimeSelector;
 
 /**
  * ===========================================
@@ -14,13 +17,46 @@ import com.mainiway.eworkpal.base.BaseTitleActivity;
  * ===========================================
  */
 
-public class AttendanceAppealActivity extends BaseTitleActivity{
+public class AttendanceAppealActivity extends BaseTitleActivity implements View.OnClickListener {
+    private TextView tv_time;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance_appeal);
         setTitle("申诉");
         showBackwardView(true);
-
+        initView();
     }
+
+    private void initView() {
+        findView(R.id.iv_time_select_button).setOnClickListener(this);
+
+        tv_time = findView(R.id.tv_time);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.iv_time_select_button://选择时间按钮
+                TimeSelector timeSelector = new TimeSelector(AttendanceAppealActivity.this, new TimeSelector.ResultHandler() {
+                    @Override
+                    public void handle(String time) {
+                        tv_time.setText(time);
+                    }
+                }, "2000-01-01 00:00", "2050-12-31 00:00");
+
+                //timeSelector.setIsLoop(false);
+                //timeSelector.disScrollUnit(TimeSelector.SCROLLTYPE.HOUR, TimeSelector.SCROLLTYPE.MINUTE);
+                //timeSelector.setMode(TimeSelector.MODE.YMD);
+                //timeSelector.setMode(TimeSelector.MODE.YMDHM);
+                timeSelector.setMode(TimeSelector.MODE.HM);
+                //timeSelector.setMode(TimeSelector.MODE.YM);
+                timeSelector.show();
+                break;
+        }
+    }
+
+    
 }
