550cc3a8a6a2ebae5de5241a4f61aeb0c0b8d1d8
zhangshuang <zhangshuang@mainiway.com>
2016-12-20 17:19:35 +0800

    登录界面接口调试

---
+++ Eworkpal/app/src/main/java/com/mainiway/eworkpal/model/UserLoginModle.java
@@ -0,0 +1,18 @@
+package com.mainiway.eworkpal.model;
+
+import java.io.Serializable;
+
+/**
+ * ===========================================
+ * 作    者：zhsh
+ * 版    本：1.0
+ * 创建日期：2016/12/20.
+ * 描    述：登录Model
+ * ===========================================
+ */
+
+public class UserLoginModle implements Serializable{
+    private static final long serialVersionUID = -9031929025134507229L;
+
+
+}
