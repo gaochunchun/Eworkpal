df9a6cad102f571f075a121b369fab022329f6a8
gao_chun <jz_gaochun@163.com>
2016-12-20 10:52:04 +0800

    注释

--- Eworkpal/app/src/main/java/com/mainiway/eworkpal/activity/attendance/StatisticsActivity.java
+++ Eworkpal/app/src/main/java/com/mainiway/eworkpal/activity/attendance/StatisticsActivity.java
@@ -14,13 +14,14 @@ import com.mainiway.eworkpal.base.BaseTitleActivity;
  * 考勤统计
  */
 
-public class StatisticsActivity extends BaseTitleActivity {
+public class StatisticsActivity extends BaseTitleActivity {/*
     //迟到 早退 未签到 未签退 外勤 设备异常
-    private LinearLayout late_layout,leave_early_layout,un_sign_in_layout,un_sign_out_layout,field_layout,equipment_abnorma_layout;
+    private LinearLayout late_layout, leave_early_layout, un_sign_in_layout, un_sign_out_layout, field_layout, equipment_abnorma_layout;
     //请假 加班 出差
-    private LinearLayout leave_layout,over_time_layout,business_travel_layout;
+    private LinearLayout leave_layout, over_time_layout, business_travel_layout;
     //签到状态
     private String SIGN_TYPE;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,16 +31,17 @@ public class StatisticsActivity extends BaseTitleActivity {
         showForwardView("团队成员");
         init();
     }
-    private void init(){
-        late_layout=findView(R.id.late_layout);
-        un_sign_in_layout=findView(R.id.un_sign_in_layout);
-        leave_layout=findView(R.id.leave_layout);
-        leave_early_layout=findView(R.id.leave_early_layout);
-        un_sign_out_layout=findView(R.id.un_sign_out_layout);
-        field_layout=findView(R.id.field_layout);
-        equipment_abnorma_layout=findView(R.id.equipment_abnorma_layout);
-        over_time_layout=findView(R.id.over_time_layout);
-        business_travel_layout=findView(R.id.business_travel_layout);
+
+    private void init() {
+        late_layout = findView(R.id.late_layout);
+        un_sign_in_layout = findView(R.id.un_sign_in_layout);
+        leave_layout = findView(R.id.leave_layout);
+        leave_early_layout = findView(R.id.leave_early_layout);
+        un_sign_out_layout = findView(R.id.un_sign_out_layout);
+        field_layout = findView(R.id.field_layout);
+        equipment_abnorma_layout = findView(R.id.equipment_abnorma_layout);
+        over_time_layout = findView(R.id.over_time_layout);
+        business_travel_layout = findView(R.id.business_travel_layout);
 
         late_layout.setOnClickListener(this);
         un_sign_in_layout.setOnClickListener(this);
@@ -58,44 +60,44 @@ public class StatisticsActivity extends BaseTitleActivity {
         super.onClick(v);
 
         //请假
-        if(v.equals(leave_layout)){
-            startActivity(new Intent(this,LeaveListActivity.class));
+        if (v.equals(leave_layout)) {
+            startActivity(new Intent(this, LeaveListActivity.class));
             return;
         }
         //加班
-        if(v.equals(over_time_layout)){
+        if (v.equals(over_time_layout)) {
 
             return;
         }
         //出差
-        if(v.equals(business_travel_layout)){
+        if (v.equals(business_travel_layout)) {
 
             return;
         }
 
-        switch (v.getId()){
-            case R.id.late_layout ://迟到
-                SIGN_TYPE="迟到";
+        switch (v.getId()) {
+            case R.id.late_layout://迟到
+                SIGN_TYPE = "迟到";
                 break;
-            case R.id.leave_early_layout ://早退
-                SIGN_TYPE="早退";
+            case R.id.leave_early_layout://早退
+                SIGN_TYPE = "早退";
                 break;
-            case R.id.un_sign_in_layout ://未签到
-                SIGN_TYPE="未签到";
+            case R.id.un_sign_in_layout://未签到
+                SIGN_TYPE = "未签到";
                 break;
-            case R.id.un_sign_out_layout ://未签退
-                SIGN_TYPE="未签退";
+            case R.id.un_sign_out_layout://未签退
+                SIGN_TYPE = "未签退";
                 break;
-            case R.id.field_layout ://外勤
-                SIGN_TYPE="外勤";
+            case R.id.field_layout://外勤
+                SIGN_TYPE = "外勤";
                 break;
-            case R.id.equipment_abnorma_layout ://设备异常
-                SIGN_TYPE="设备异常";
+            case R.id.equipment_abnorma_layout://设备异常
+                SIGN_TYPE = "设备异常";
                 break;
             default:
-                Intent intent =new Intent (this,LateListActivity.class);
-                intent.putExtra("SIGN_TYPE",SIGN_TYPE);
+                Intent intent = new Intent(this, LateListActivity.class);
+                intent.putExtra("SIGN_TYPE", SIGN_TYPE);
                 startActivity(intent);
         }
-    }
+    }*/
 }
