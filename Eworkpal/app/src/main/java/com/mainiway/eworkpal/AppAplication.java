179091b47f140d93e4a480ba98249f40c2cef363
gaochunchun <jz_gaochun@163.com>
2017-03-13 10:45:46 +0800

    移除ImageLoader初始化

--- Eworkpal/app/src/main/java/com/mainiway/eworkpal/AppAplication.java
+++ Eworkpal/app/src/main/java/com/mainiway/eworkpal/AppAplication.java
@@ -14,7 +14,6 @@ import com.mainiway.okhttp.GenericRequestManager;
 public class AppAplication extends Application {
 
     private static Context mContext;
-    //private  DisplayImageOptions options;
 
     //图片缓存路径
     //public static File cacheDir;
@@ -33,60 +32,16 @@ public class AppAplication extends Application {
         ConfigManager.initialize(getApplicationContext());
 
         // 初始化网络配置
-        //final GlobalConfigManager globalConfigManager = GlobalConfigManager.getInstance();
-        //GenericRequestManager.initialize(getApplicationContext(), globalConfigManager.getServerHost());
         GenericRequestManager.initialize(this);
-
-        // 初始化ImageLoader
-        //initImageLoader(getApplicationContext());
 
         //初始化SDcard目录
         //FileUtils.initFileDir();
 
     }
 
-
     public static Context getContext() {
         return mContext;
     }
-
-
-    /**
-     * init the ImageLoader
-     * @param context
-     */
-    /*public void initImageLoader(Context context) {
-
-        com.nostra13.universalimageloader.utils.L.writeLogs(false);// 关闭ImageLoaderLog日志
-
-        cacheDir = StorageUtils.getOwnCacheDirectory(this, CACHE_DIR + CACHE_IMG_DOC);//图像缓存路径
-
-        //配置加载Option
-        options = new DisplayImageOptions.Builder()
-        .showImageOnLoading(R.mipmap.ic_default)  //加载过程中
-        .showImageForEmptyUri(R.mipmap.ic_default)//uri为空时
-        .showImageOnFail(R.mipmap.ic_default)     //加载失败时
-        .cacheInMemory(true)
-        .cacheOnDisk(true)
-        .bitmapConfig(Bitmap.Config.RGB_565)    //图片压缩质量参数
-        .build();
-
-        //配置ImageLoader
-        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
-                context).threadPriority(Thread.NORM_PRIORITY - 1)
-                .denyCacheImageMultipleSizesInMemory()
-                .diskCache(new UnlimitedDiskCache(cacheDir))
-                .diskCacheFileNameGenerator(new HashCodeFileNameGenerator())
-                .diskCacheSize(100 * 1024 * 1024) // 100 Mb
-                //.diskCacheFileNameGenerator(new Md5FileNameGenerator())
-                .tasksProcessingOrder(QueueProcessingType.LIFO)
-                .memoryCache(new WeakMemoryCache())
-                .defaultDisplayImageOptions(options)
-                .build();
-
-        // Initialize ImageLoader with configuration.
-        ImageLoader.getInstance().init(config);
-    }*/
 
 
     /**
